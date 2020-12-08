bb19b491a86ead1cd519ee98c24ca6688a3805e6
Olivier <olivier.elophe@gmail.com>
2020-12-08 09:27:48 +0100

    Exemple de Narusturm

---
+++ narutsturm.py
@@ -0,0 +1,51 @@
+###########################################
+#           SNT - M. Elophe - 2020        #
+###########################################
+
+# Ici on va importer les modules que nous allons utiliser.
+# C'est comme le from robot import * de France IOI
+import pygame
+from pygame.locals import *
+
+# Ouverture de la fenêtre Pygame
+TAILLE_FENETRE = (640, 480)
+fenetre = pygame.display.set_mode(TAILLE_FENETRE)
+# On peut ajouter un titre à la fenêtre.
+pygame.display.set_caption("Narusturm !")
+
+# Couleur de fond au format RGB : (Rouge, Vert, Bleu)
+# En l'occurence, ici, j'ai jeté mon dévolu sur un petit violet pâle pas trop agrgessif.
+couleur = (92, 107, 192)
+fenetre.fill(couleur)
+
+# Chargement du personnage
+perso = pygame.image.load("images/narut0.png").convert_alpha()
+# On redimensionne l'image en 20 px de large et 50 de haut
+perso = pygame.transform.scale(perso, (20, 50))
+# On colle le personnage
+fenetre.blit(perso, (200,300))
+position = perso.get_rect()
+# Il faut penser à rafraichir l'écran ! (Surtout en été)
+pygame.display.flip()
+
+# Boucle de jeu, "infinie", parce que le jeu doit continuer tant que je ne l'arrête pas
+continuer = True
+
+while continuer:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            continuer = 0
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                position = position.move(5, 0)
+            if event.key == K_DOWN:
+                position = position.move(0, 5)
+        #ICI RAJOUTER LES AUTRES DEPLACEMENT
+
+
+
+    fenetre.fill(couleur)
+    fenetre.blit(perso, position)
+    pygame.display.flip()
+

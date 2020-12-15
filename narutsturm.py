525eaeb4535886634fdbd7c36456f29f01bc782c
Olivier <olivier.elophe@gmail.com>
2020-12-15 12:54:14 +0100

    affichage structure

--- narutsturm.py
+++ narutsturm.py
@@ -28,15 +28,19 @@ position = perso.get_rect()
 # Il faut penser à rafraichir l'écran ! (Surtout en été)
 pygame.display.flip()
 
+#########
+mur = pygame.image.load("images/mur.jpg").convert_alpha()
+sol = pygame.image.load("images/gazon.jpg").convert_alpha()
+# On redimensionne l'image en 20 px de large et 50 de haut
+sol = pygame.transform.scale(sol, (32, 32))
 ####################################
 # Lecture de la map
 #################################"
 with open("maps/map1.txt", 'r') as file:
     data = file.read()
     print(data)
-
-
-
+structure = data.split("\n")
+print(structure)
 
 # Boucle de jeu, "infinie", parce que le jeu doit continuer tant que je ne l'arrête pas
 continuer = True
@@ -49,7 +53,25 @@ while continuer:
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 position = position.move(5, 0)
-        #ICI RAJOUTER LES AUTRES DEPLACEMENT
+            if event.key == K_LEFT:
+                position = position.move(-5, 0)
+            if event.key == K_UP:
+                position = position.move(0, -5)
+            if event.key == K_DOWN:
+                position = position.move(0, 5)
     fenetre.fill(couleur)
+
+    num_ligne = 0
+    num_col = 0
+    for ligne in structure:
+        num_col = 0
+        for case in ligne:
+            if case == 'm':
+                fenetre.blit(mur, (num_col * 32, num_ligne * 32))
+            else:
+               fenetre.blit(sol, (num_col * 32, num_ligne * 32))
+            num_col += 1
+        num_ligne += 1
+        #ICI RAJOUTER LES AUTRES DEPLACEMENT
     fenetre.blit(perso, position)
     pygame.display.flip()
